81c4c2f837fbac5ebf8fa650a2f50a8adaa9034f
Ivan Sumlikin <ivanbsd@Ivans-MacBook-Air.local>
2018-01-19 00:14:45 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Open Sans:400,700', 'sans-serif']
+  }
+});
+
+require('./style/app.scss');
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
