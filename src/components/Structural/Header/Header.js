0fcaf1214c9002cd31d0fb0d08632e112b1ced8c
Ivan Sumlikin <ivanbsd@Ivans-MacBook-Air.local>
2018-01-21 23:29:30 +0700

    Add geeral styles and use Yarn

--- src/components/Structural/Header/Header.js
+++ src/components/Structural/Header/Header.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import { Nav, Navbar, NavItem, 
-  NavDropdown, MenuItem, FormGroup, FormControl } from 'react-bootstrap';
+  NavDropdown, MenuItem, FormGroup, FormControl,
+  Image, Button } from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
+import './style.scss';
 
 const Header = () => (
-  <header>
-
-    <Navbar fluid collapseOnSelect>
+  <header id='appHeader'>
+    <Navbar fixedTop fluid collapseOnSelect>
       <Navbar.Header>
         <Navbar.Brand>
-          <a href='#brand'>React-Bootstrap</a>
+          <a href='#brand'>
+            <Image src='../../../../images/logo_v2.0.svg' />
+          </a>
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav>
           <FormGroup
-            controlId='formBasicText'
+            id='header-search-form'
           >
             <FormControl
               type='text'
               placeholder='Search'
             />
+            <Button>
+              <FontAwesome name='search' />
+            </Button>
           </FormGroup>
         </Nav>
         <Nav pullRight>
@@ -34,19 +40,21 @@ const Header = () => (
           <NavItem eventKey={3} href='#'>
             Blog
           </NavItem>
-          <NavDropdown eventKey={3} title='Dropdown' id='basic-nav-dropdown'>
-            <MenuItem eventKey={3.1}>Action</MenuItem>
-            <MenuItem eventKey={3.2}>Another action</MenuItem>
-            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+          <NavDropdown noCaret 
+            eventKey={3} title='Language' 
+            id='basic-nav-dropdown'
+          >
+            <MenuItem eventKey={3.1}>English</MenuItem>
+            <MenuItem eventKey={3.2}>Chinese</MenuItem>
+            <MenuItem eventKey={3.3}>Espanol</MenuItem>
           </NavDropdown>
-          <NavDropdown eventKey={3} title='Dropdown' id='basic-nav-dropdown'>
-            <MenuItem eventKey={3.1}>Action</MenuItem>
-            <MenuItem eventKey={3.2}>Another action</MenuItem>
-            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+          <NavDropdown noCaret 
+            eventKey={3} title='Currency' 
+            id='basic-nav-dropdown'
+          >
+            <MenuItem eventKey={3.1}>USD</MenuItem>
+            <MenuItem eventKey={3.2}>BTC</MenuItem>
+            <MenuItem eventKey={3.3}>EUR</MenuItem>
           </NavDropdown>
           <NavItem eventKey={3}>
             <FontAwesome name='user-circle-o' size='2x' tag='i'/>
